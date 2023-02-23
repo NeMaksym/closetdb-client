59c0653905546dc78c13b8e9094e9df3e8f31e43
Maksym Netreba <51268714+NeMaksym@users.noreply.github.com>
2023-02-23 23:05:17 +0200

    Integrate Material UI

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import CssBaseline from '@mui/material/CssBaseline';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -8,6 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 );
